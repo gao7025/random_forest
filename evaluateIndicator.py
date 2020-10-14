0c5bf2d8db5a769c31f43dff3d8856e0577f8b52
gao7025 <gao7025@126.com>
2020-10-14 10:12:36 +0800

    maintenance

---
+++ evaluateIndicator.py
@@ -0,0 +1,98 @@
+
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+import warnings
+
+class evaluateIndicator():
+
+    def __init__(self):
+        pass
+
+    # 交叉验证
+    def plot_cross_val(self, rf4, train_x, train_y, cv_num, path_out):
+        warnings.filterwarnings("ignore")
+        plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+        plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+        evaluate_vars = ['roc_auc', 'precision', 'recall', 'f1']
+        fig = plt.figure()
+        for plot_num in range(len(evaluate_vars)):
+            ax1 = fig.add_subplot(2, 2, plot_num + 1)
+            plt.subplots_adjust(left=None, bottom=None, right=None, top=None,
+                                wspace=0.3, hspace=0.5)
+            try:
+                scores = cross_val_score(rf4, train_x, train_y, cv=cv_num, scoring=evaluate_vars[plot_num])
+            except ValueError:
+                scores = np.zeros(10)
+            plt.plot(range(10), scores)
+            plt.xlabel('num of cv')
+            plt.ylabel(evaluate_vars[plot_num])
+            plt.xticks(np.arange(0, 10, 1), fontsize=6)
+            plt.yticks(np.arange(0, 1.1, 0.2), fontsize=8)
+            plt.show()
+            tt = 'plot of ' + str(evaluate_vars[plot_num])
+            ax1.set_title(tt, fontsize=10)
+        plt.savefig(path_out, bbox_inches='tight', dpi=300)  # bbox_inches='tight'帮助删除图片空白部分
+        plt.show()
+
+    def predict_result(self, rf4, train_x, train_y, test_x, test_y):
+        # 拟合模型并预测
+        rf4.fit(train_x,train_y)
+        train_prob = rf4.predict_proba(train_x)
+        test_prob = rf4.predict_proba(test_x)
+        test_pred = rf4.predict(test_x)
+        obj1, result_report = self.evaluate_result(test_y, test_pred)
+        self.plot_roc(test_y, test_prob[:, 1])
+        self.plot_pr(train_y, train_prob, test_y, test_prob)
+        return obj1, result_report
+
+    def evaluate_result(self, test_y, test_pred):
+        # 评价指标1：混淆矩阵相关
+        obj1 = confusion_matrix(test_y, test_pred)
+        print('confusion_matrix\n', obj1)
+        # 1.1综合评估
+        result_report = classification_report(test_y,test_pred,digits=5)
+        print(result_report)
+        # 1.2分别查看
+        print('accuracy:{}'.format(accuracy_score(test_y, test_pred)))
+        print('precision:{}'.format(precision_score(test_y, test_pred)))
+        print('recall:{}'.format(recall_score(test_y, test_pred)))
+        print('f1-score:{}'.format(f1_score(test_y, test_pred)))
+        return obj1, result_report
+
+    # 评价指标2：roc
+    def plot_roc(self, test_y, test_prob):
+        fpr,tpr,threshold=roc_curve(test_y,test_prob)
+        roc_auc=auc(fpr,tpr)
+        # 画roc曲线
+        plt.figure(figsize=(10,10))
+        plt.plot(fpr, tpr, color='darkorange',lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=12)
+        plt.xlabel('False Positive Rate',fontsize=12)
+        plt.ylabel('True Positive Rate',fontsize=12)
+        plt.title('Receiver operating characteristic example',fontsize=12)
+        plt.legend(loc="lower right",fontsize=12)
+        plt.show()
+
+    # 评价指标3：pr曲线
+    def plot_pr(self, train_y, train_prob,test_y,test_prob):
+        def draw_pr(y_true, y_pred, label=None):
+            precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
+            plt.plot(recall, precision, linewidth=2, label=label)
+            plt.plot([0, 1], [0, 1], 'k--')
+            plt.axis([-0.05, 1.05, -0.05, 1.05])
+            plt.xlabel("Recall Rate")
+            plt.ylabel("Precision Rate")
+        draw_pr(train_y, train_prob, 'rf_train')
+        draw_pr(test_y, test_prob, 'rf_test')
+        plt.legend()
+        plt.show()
---
+++ randomForest.py
@@ -0,0 +1,95 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import roc_curve,auc,roc_auc_score,confusion_matrix,accuracy_score,precision_score,recall_score,f1_score
+from evaluateIndicator import evaluateIndicator
+import warnings
+warnings.filterwarnings("ignore")
+path_out = 'E:/programGao/csdnProgram'
+
+# 加载数据并划分数据集
+data = pd.read_excel('E:/programGao/creditdata.xlsx','dataset')
+x = data.drop(['uid','flag'],axis=1)
+y = data['flag']
+train_x,test_x,train_y,test_y = train_test_split(x,y,test_size=0.3,random_state=423)
+
+# 0.查看初始效果
+rf0 = RandomForestClassifier(oob_score=True, random_state=10)
+rf0.fit(train_x,train_y)
+y_predprob = rf0.predict_proba(test_x)[:,1]
+print('oob_score : %f ,auc : %f' % (rf0.oob_score_,roc_auc_score(test_y, y_predprob)))
+
+
+# 1.迭代次数
+param_test1 = {'n_estimators':range(10,101,10)}
+gsearch1 = GridSearchCV(estimator = RandomForestClassifier(min_samples_split=20,
+                                  min_samples_leaf=10,max_depth=5,max_features='sqrt' ,random_state=10),
+                       param_grid = param_test1, scoring='roc_auc',cv=5)
+gsearch1.fit(train_x,train_y)
+means = gsearch1.cv_results_['mean_test_score']
+std = gsearch1.cv_results_['std_test_score']
+params = gsearch1.cv_results_['params']
+for mean,std,param in zip(means,std,params):
+    print("mean : %f std : %f %r" % (mean,std,param))
+print('best_params :',gsearch1.best_params_)
+
+# 2.最大深度和内部节点再划分所需最小样本数
+param_test2 = {'max_depth':range(3,10,1), 'min_samples_split':range(10,101,10)}
+gsearch2 = GridSearchCV(estimator = RandomForestClassifier(n_estimators= 40,
+                                  min_samples_leaf=10,max_features='sqrt' ,oob_score=True, random_state=10),
+   param_grid = param_test2, scoring='roc_auc',iid=False, cv=5)
+gsearch2.fit(train_x,train_y)
+means = gsearch2.cv_results_['mean_test_score']
+std = gsearch2.cv_results_['std_test_score']
+params = gsearch2.cv_results_['params']
+for mean,std,param in zip(means,std,params):
+    print("mean : %f std : %f %r" % (mean,std,param))
+print('best_params :',gsearch2.best_params_)
+
+# 查看此时结果
+rf2 = RandomForestClassifier(n_estimators= 40, max_depth=5, min_samples_split=70,
+                                  min_samples_leaf=10,max_features='sqrt' ,oob_score=True, random_state=10)
+rf2.fit(train_x,train_y)
+y_predprob2 = rf2.predict_proba(test_x)[:,1]
+print('oob_score : %f ,auc : %f' % (rf2.oob_score_,roc_auc_score(test_y, y_predprob2)))
+
+# 3.内部节点再划分所需最小样本数和叶子节点最少样本数
+param_test3 = {'min_samples_split':range(10,101,10), 'min_samples_leaf':range(5,51,5)}
+gsearch3 = GridSearchCV(estimator = RandomForestClassifier(n_estimators= 40, max_depth=5,
+                                  max_features='sqrt' ,oob_score=True, random_state=10),
+   param_grid = param_test3, scoring='roc_auc',iid=False, cv=5)
+gsearch3.fit(train_x,train_y)
+means = gsearch3.cv_results_['mean_test_score']
+std = gsearch3.cv_results_['std_test_score']
+params = gsearch3.cv_results_['params']
+for mean,std,param in zip(means,std,params):
+    print("mean : %f std : %f %r" % (mean,std,param))
+print('best_params :',gsearch3.best_params_)
+
+# 4.最大特征数
+param_test4 = {'max_features':range(3,11,1)}
+gsearch4 = GridSearchCV(estimator = RandomForestClassifier(n_estimators= 40, max_depth=5, min_samples_split=70,
+                                  min_samples_leaf=10 ,oob_score=True, random_state=10),
+   param_grid = param_test4, scoring='roc_auc',iid=False, cv=5)
+gsearch4.fit(train_x,train_y)
+means = gsearch4.cv_results_['mean_test_score']
+std = gsearch4.cv_results_['std_test_score']
+params = gsearch4.cv_results_['params']
+for mean,std,param in zip(means,std,params):
+    print("mean : %f std : %f %r" % (mean,std,param))
+print('best_params :',gsearch4.best_params_)
+# 查看此时结果
+rf4 = RandomForestClassifier(n_estimators= 40, max_depth=5, min_samples_split=70,
+                                  min_samples_leaf=10,max_features=3 ,oob_score=True, random_state=10)
+rf4.fit(train_x,train_y)
+y_predprob4 = rf4.predict_proba(test_x)[:,1]
+print('oob_score : %f ,auc : %f' % (rf4.oob_score_,roc_auc_score(test_y, y_predprob4)))
+
+# 查看交叉验证结果并进行预测与评估
+el = evaluateIndicator()
+el.plot_cross_val(rf4, train_x, train_y, 10, path_out)
+obj1, result_report = el.predict_result(rf4, train_x, train_y, test_x, test_y)
+
